8814223ed39f67311060f773d8810036852bd496
Saket <pure.vintage.skt@gmail.com>
2011-07-21 20:06:46 +0530

    categories being populated, the links are not functional yet

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
-from recipes.models import RecipeDump
+from recipes.models import RecipeDump, Category
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import Context, loader, RequestContext
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+recipesobj = RecipeDump.objects.all()
+categobj = Category.objects.all() 
 
 def render(request, template, context):
     return render_to_response(template,context,context_instance = RequestContext(request))
@@ -11,4 +15,15 @@ def hello(request):
     return HttpResponse("Hello world")
 
 def base(request):
-    return render(request, 'base.html', 'none')
+    return render(request, 'base.html', {'recipesobj':recipesobj,'categobj':categobj})
+
+def datadisp(request):
+    return render(request, 'base.html', none)
+ 
+
+def detail(request, recipe_id=None):
+    try:
+        p = RecipeDump.objects.filter(slug=recipe_id)[0]
+    except IndexError:
+        raise Http404
+    return render(request, 'detail.html', {'recipe': p})    
