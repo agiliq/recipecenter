2063396440a302dbf90088af2a08d6f04da1a24e
Saket <pure.vintage.skt@gmail.com>
2011-07-28 15:22:37 +0530

    Search Folder Added

---
+++ recipes/search_indexes.py
@@ -0,0 +1,16 @@
+import datetime
+from haystack.indexes import *
+from haystack import site
+from recipes.models import RecipeDump
+
+class RecipeIndex(SearchIndex):
+  text = CharField(document=True, use_template=True)
+  category = CharField(model_attr='category')
+  name = CharField(model_attr='name')
+  ingredients = CharField(model_attr='ingredients')
+  slug = CharField(model_attr='slug')
+  
+  def index_queryset(self):
+    return RecipeIndex.objects.filter(pub_date_lte=datetime.datetime.now())
+
+site.register(RecipeDump, RecipeIndex)
