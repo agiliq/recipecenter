0e0cf2200728da81a5f251b9465fc18734e139e7
krvc <krvc.it@gmail.com>
2013-05-20 18:47:05 +0530

    Modified gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,4 @@ bind = "0.0.0.0:8000"
 workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
 loglevel = "info"
 proc_name = "recipecenter"
+os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
