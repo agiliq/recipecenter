22601bbff0d2c5654c7c65e5a9d5a725e568a9e9
Jakh Daven <tuxcanfly@gmail.com>
2013-05-20 18:47:47 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,4 +4,4 @@ bind = "0.0.0.0:8000"
 workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
 loglevel = "info"
 proc_name = "recipecenter"
-os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+pythonpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
