6bc068e36a46cee3c583f7ea856126cc4f0b68de
krvc <krvc.it@gmail.com>
2013-05-20 19:09:52 +0530

    Modified install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 cp local_settings.py-dist local_settings.py
-python manage.py synchdb --migrate
+python manage.py syncdb --migrate
 python manage.py run_gunicorn -c gunicorn.conf.py
 
 
