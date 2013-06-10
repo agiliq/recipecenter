875dbaa87b78e587768cc104d0cd2c1bda60cf8d
krvc <krvc.it@gmail.com>
2013-06-10 13:00:31 +0530

    Removed unused imports

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from recipes import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from djangoratings.views import AddRatingFromModel
 from django.conf import settings
 # Uncomment the next two lines to enable the admin:
