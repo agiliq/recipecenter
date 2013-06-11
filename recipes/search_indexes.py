3a71c491bd5c03c75b5c437d6f2420356b5e5d10
krvc <krvc.it@gmail.com>
2013-06-11 18:58:47 +0530

    pep8 fixes

--- recipes/search_indexes.py
+++ recipes/search_indexes.py
@@ -3,14 +3,15 @@ from haystack.indexes import *
 from haystack import site
 from recipes.models import Recipe
 
+
 class RecipeIndex(SearchIndex):
-  text = CharField(document=True, use_template=True)
-  category = CharField(model_attr='category')
-  name = CharField(model_attr='name')
-  ingredients = CharField(model_attr='ingredients')
-  slug = CharField(model_attr='slug')
-  
-  def index_queryset(self):
-    return RecipeIndex.objects.filter(pub_date_lte=datetime.datetime.now())
+    text = CharField(document=True, use_template=True)
+    category = CharField(model_attr='category')
+    name = CharField(model_attr='name')
+    ingredients = CharField(model_attr='ingredients')
+    slug = CharField(model_attr='slug')
+
+    def index_queryset(self):
+        return RecipeIndex.objects.filter(pub_date_lte=datetime.datetime.now())
 
 site.register(Recipe, RecipeIndex)
