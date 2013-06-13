728aed308ff1b3c4760e817f055e559fb8f44b59
krvc <krvc.it@gmail.com>
2013-06-13 17:00:33 +0530

    added migration  files

---
+++ recipes/migrations/0003_auto__add_field_recipe_is_featured.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Recipe.is_featured'
+        db.add_column(u'recipes_recipe', 'is_featured',
+                      self.gf('django.db.models.fields.BooleanField')(default=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Recipe.is_featured'
+        db.delete_column(u'recipes_recipe', 'is_featured')
+
+
+    models = {
+        u'recipes.category': {
+            'Meta': {'object_name': 'Category'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'})
+        },
+        u'recipes.recipe': {
+            'Meta': {'object_name': 'Recipe'},
+            'added_by': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'bar': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'baz': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'blah': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['recipes.Category']", 'null': 'True', 'blank': 'True'}),
+            'foo': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'ingredients': ('django.db.models.fields.TextField', [], {}),
+            'instructions': ('django.db.models.fields.TextField', [], {}),
+            'is_featured': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'}),
+            'something': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['recipes']
