19d25ac2af6e732784f5c0f8e6a3736c17bcac4d
krvc <krvc.it@gmail.com>
2013-06-16 00:44:06 +0530

    Removed urls for ratings and comments

--- urls.py
+++ urls.py
@@ -8,19 +8,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url('^hello/$','recipes.views.hello', name='hello'),
+    url('^hello/$', 'recipes.views.hello', name='hello'),
     url('^$', 'recipes.views.base', name='base'),
     url('^category/(?P<category_slug>[\w-]+)/$', 'recipes.views.category', name='category'),
     url('^detail/(?P<recipe_id>[\w-]+)/$', 'recipes.views.detail', name='recipe_detail'),
-    (r'^comments/', include('django.contrib.comments.urls')),
-    url(r'^rate-my-post/(?P<object_id>\d+)/(?P<score>\d+)/$', AddRatingFromModel(), {
-        'app_label': 'recipes',
-        'model': 'Recipe',
-        'field_name': 'rating',
-    },name='vote'),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^search/', include('haystack.urls')),
 
