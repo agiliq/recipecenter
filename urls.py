c354b991d09edc1127f0dc2968e4d61964e5ac2a
krvc <krvc.it@gmail.com>
2013-06-17 03:12:49 +0530

    Removed unwanted imports

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from djangoratings.views import AddRatingFromModel
 from django.conf import settings
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
