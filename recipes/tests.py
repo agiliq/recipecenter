2a93e877de8299e470d55612f23c8555dde9dc73
krvc <krvc.it@gmail.com>
2013-06-17 02:38:32 +0530

    Added test test_startpage

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,11 +6,23 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SiteTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_startpage(self):
+        response = self.c.get(reverse("hello"))
+        self.assertEqual(response.status_code, 200)
+
+
+# class SimpleTest(TestCase):
+#     def test_basic_addition(self):
+#         """
+#         Tests that 1 + 1 always equals 2.
+#         """
+#         self.assertEqual(1 + 1, 2)
