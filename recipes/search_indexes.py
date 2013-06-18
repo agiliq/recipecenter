05c19f473989a4445673495b350b497c4933cf74
krvc <krvc.it@gmail.com>
2013-06-18 12:55:38 +0530

    Modified search_indexes.py to newer version

--- recipes/search_indexes.py
+++ recipes/search_indexes.py
@@ -1,15 +1,15 @@
 import datetime
-from haystack.indexes import *
+from haystack import indexes
 from haystack import site
 from recipes.models import Recipe
 
 
-class RecipeIndex(SearchIndex):
-    text = CharField(document=True, use_template=True)
-    category = CharField(model_attr='category')
-    name = CharField(model_attr='name')
-    ingredients = CharField(model_attr='ingredients')
-    slug = CharField(model_attr='slug')
+class RecipeIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    category = indexes.CharField(model_attr='category')
+    name = indexes.CharField(model_attr='name')
+    ingredients = indexes.CharField(model_attr='ingredients')
+    slug = indexes.CharField(model_attr='slug')
 
     def index_queryset(self):
         return RecipeIndex.objects.filter(pub_date_lte=datetime.datetime.now())
