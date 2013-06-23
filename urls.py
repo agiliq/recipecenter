d148834067a4689dd6e56b4f94c60467701b34a0
krvc <krvc.it@gmail.com>
2013-06-24 01:52:59 +0530

    Removed reviews.html as reviews are removed and removed unwanted files

--- urls.py
+++ urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import patterns, include, url
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
