decb3d066b4ff905a53405d633c372c47039e15d
krvc <krvc.it@gmail.com>
2013-06-26 16:35:06 +0530

    removed MEDIA_ROOT from STATICFILES_DIRS

--- settings.py
+++ settings.py
@@ -81,7 +81,6 @@ ADMIN_MEDIA_PREFIX = '/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    MEDIA_ROOT,
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
