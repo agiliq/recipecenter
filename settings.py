d393ef6704ab5edac714237f03d9ed44846362ea
krvc <krvc.it@gmail.com>
2013-06-26 17:30:09 +0530

    Modified settings file

--- settings.py
+++ settings.py
@@ -58,7 +58,7 @@ MEDIA_URL = '/static_media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
