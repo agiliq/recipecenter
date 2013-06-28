a5d1cc9266a182e90f5aaf740395770743544998
krvc <krvc.it@gmail.com>
2013-06-28 17:29:18 +0530

    Modified STATIC_ROOT and MEDIA_ROOT

--- settings/common.py
+++ settings/common.py
@@ -47,7 +47,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -58,7 +58,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
