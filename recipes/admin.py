da3738b136497fc89f4e2836bcd9af465f1a3649
krvc <krvc.it@gmail.com>
2013-07-24 20:42:53 +0530

    Added search and categoty field to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -4,7 +4,10 @@ from .models import Recipe, Category
 
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ["name", "is_featured"]
+    list_display = ["name", "category", "is_featured"]
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Category)
